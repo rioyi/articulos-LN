d16b2bc351b0be22bd31cc154e564e81795d88ee
rioyi <wilbert.angarita@gmail.com>
2020-11-25 19:27:47 -0300

    set config for SSR

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0)
+  
+  const increment = () => {
+    console.log('veamos')
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hola</h1>
+      <p> {count} </p>
+      <buttom onClick={increment}>Increment</buttom>
+      <buttom onClick={decrement}>Decrement</buttom>
+    </>
   );
 }
 
